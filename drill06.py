d905856dd65a3c945e3a773fde2fa72c0ca8666d
승범 김 <wewe231223@kpu.ac.kr>
2023-10-04 22:21:36 +0900

    reformat

--- drill06.py
+++ drill06.py
@@ -21,8 +21,8 @@ def handle_events():
             running = False
         elif event.type == SDL_MOUSEMOTION:
             mx, my = event.x, TUK_HEIGHT - 1 - event.y
-        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT: # 마우스 클릭이 있으면..
-            points.append((event.x, TUK_HEIGHT - 1 - event.y)) # 클릭된 위치를 새로운 점으로 추가.
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:  # 마우스 클릭이 있으면..
+            points.append((event.x, TUK_HEIGHT - 1 - event.y))  # 클릭된 위치를 새로운 점으로 추가.
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -42,7 +42,7 @@ def reset_world():
     frame = 0
     action = 3
 
-    points = [  ]
+    points = []
     set_new_target_arrow()
 
 def set_new_target_arrow():
@@ -52,7 +52,7 @@ def set_new_target_arrow():
 
     global target_exists
 
-    if points: # points 리스트안에 남아있는 점이 있으면,
+    if points:  # points 리스트안에 남아있는 점이 있으면,
         sx, sy = cx, cy  # p1 : 시작점
         # hx, hy = 50, 50
         hx, hy = points[0]  # p2 : 끝점.
@@ -92,11 +92,11 @@ def update_world():
             cx = (1 - t) * sx + t * hx  # cx 는 시작 x 와 끌 x 를 1-t:t 의 비율로 섞은 위치
             cy = (1 - t) * sy + t * hy
             t += 0.001
-        else: # 목표지점에 도달하면
-            cx, cy = hx, hy # 캐릭터 위치를 기존 목적지 위치와  강제로 정확히 일치시킴.
-            del points[0] # 목표지점에 왔기 때문에, 더 이상 필요없는 점을 삭제
+        else:  # 목표지점에 도달하면
+            cx, cy = hx, hy  # 캐릭터 위치를 기존 목적지 위치와  강제로 정확히 일치시킴.
+            del points[0]  # 목표지점에 왔기 때문에, 더 이상 필요없는 점을 삭제
             set_new_target_arrow()
-    elif points: # 목표 지점에 없는 상황에서, 새로운 목표 지점이 생기면...
+    elif points:  # 목표 지점에 없는 상황에서, 새로운 목표 지점이 생기면...
         set_new_target_arrow()
 
 
